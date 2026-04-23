f010ad3da3022a34640c2d109db118168f1b2034
jayasuriyasr <jayasuriyasr006@gmail.com>
2026-04-23 13:26:54 +0530

    fixed some order model issue and updated shipping time automatically

--- ecomprj/payment/models.py
+++ ecomprj/payment/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from store.models import Product
+from django.db.models.signals import post_save,pre_save
+from django.dispatch import receiver
+import datetime
 
 # Create your models here.
 class ShippingAddress(models.Model):
@@ -26,15 +29,25 @@ class Order(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     full_name = models.CharField(max_length=50)
-    email = models.CharField(max_length=100)
-    shipping_address = models.EmailField(max_length=2500)
+    email = models.EmailField(max_length=500)
+    shipping_address = models.TextField(max_length=500)
     amount_paid = models.DecimalField(max_digits=10, decimal_places=3)
     date_ordered = models.DateTimeField(auto_now_add=True)
     shipped=models.BooleanField(default=False)
-
+    date_shipped = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return f"Order - {self.id}"
+
+# to add shipping date 
+@receiver(pre_save,sender=Order)
+
+def set_shipping_date(sender,instance,**kwargs):
+    if instance.pk:
+        now = datetime.datetime.now()
+        obj = sender._default_manager.get(pk=instance.pk)
+        if instance.shipped and not obj.shipped:
+            instance.date_shipped = now
 
 class OrderItem(models.Model):
 
