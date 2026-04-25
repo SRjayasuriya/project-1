2f90512394996b10630a3a2d330556e1f832e0c7
jayasuriyasr <jayasuriyasr006@gmail.com>
2026-04-25 22:38:10 +0530

    updated some search engine

--- ecomprj/store/trie.py
+++ ecomprj/store/trie.py
@@ -1,8 +1,19 @@
 from collections import defaultdict
 class Trie:
     def __init__(self):
-        pass
-    def insert(self):
-        pass
-    def search(self):
-        pass
+        self.children = defaultdict(Trie)
+        self.is_end_of_word = False
+
+    def search(self, word):
+        current = self
+        for char in word:
+            if char not in current.children:
+                return False
+            current = current.children[char]
+        return current.is_end_of_word
+
+    def insert(self, word):
+        current = self
+        for char in word:
+            current = current.children[char]
+        current.is_end_of_word = True
